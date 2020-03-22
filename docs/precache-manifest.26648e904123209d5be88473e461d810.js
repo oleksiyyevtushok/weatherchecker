cef82c81ae2cbf4a51e523979cee377972a31939
alexeyyevtushok <alexey.yevtushok@gmail.com>
2020-03-22 18:25:25 +0200

    docs fold created

---
+++ docs/precache-manifest.26648e904123209d5be88473e461d810.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d064b8b32992bc1e55098e9e2628b9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87c4831e9cbbb75ef7ce",
+    "url": "/static/css/main.36e91cd3.chunk.css"
+  },
+  {
+    "revision": "b72cef2fbb84fa16f654",
+    "url": "/static/js/2.37acad76.chunk.js"
+  },
+  {
+    "revision": "87c4831e9cbbb75ef7ce",
+    "url": "/static/js/main.4bf9028e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
