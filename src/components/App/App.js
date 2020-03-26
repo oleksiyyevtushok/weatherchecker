353cc8bcea85873452b8f62fb24c1b4ca5a37692
alexeyyevtushok <alexey.yevtushok@gmail.com>
2020-03-26 19:17:55 +0200

    add Google Analytics

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import ReactGA from 'react-ga';
 
 import Header from '../Header/Header';
 import Card from '../Card/Card';
 import Search from '../Search/Search';
+
+ReactGA.initialize('UA-161537135-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+ReactGA.set({dimension1: "Member"});
 
 function App() {
   return (
